f2469c8620c00d1e668c5478f0a6a9efe8d80236
Frank Berger <fberger@b-factor.de>
2018-09-13 17:28:04 +0200

    update test config

--- Tests/Unit/ViewHelpers/SimplelinkViewHelperTest.php
+++ Tests/Unit/ViewHelpers/SimplelinkViewHelperTest.php
@@ -9,19 +9,37 @@
 namespace SUDHAUS7\Sudhaus7Formframework\Tests\Unit\ViewHelpers;
 
 use Nimut\TestingFramework\TestCase\UnitTestCase;
+use SUDHAUS7\Sudhaus7Formframework\ViewHelpers\SimplelinkViewHelper;
 
 
-class SimplelinkViewHelperTest
+class SimplelinkViewHelperTest extends UnitTestCase
 {
     
     protected $mockedContentObjectRenderer;
     protected $mockedViewHelper;
+    protected $testtext = 'This [Text] is linked';
     
     /**
      * Set up
      */
     public function setUp()
     {
+        
+        $this->mockedViewHelper = $this->getAccessibleMock(SimplelinkViewHelper::class,['init','renderChildren']);
+        $this->mockedContentObjectRenderer = $this->getAccessibleMock(\TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer::class, [ 'typoLink']);
+        
+    }
     
+    /**
+     * @test
+     */
+    public function internalPageIsUsed()
+    {
+        $url = 123;
+        $result = ['parameter'=>$url];
+        //$this->mockedContentObjectRenderer->expects($this->once())->method('typoLink_URL')->with($result);
+        $this->mockedViewHelper->_set($url);
+        $this->mockedViewHelper->_set($url);
+        $result = $this->mockedViewHelper()->render();
     }
 }
