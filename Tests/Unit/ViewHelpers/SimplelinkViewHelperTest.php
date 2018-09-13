35010bcd320919cc643f55338c8531e33114ff3d
Frank Berger <fberger@b-factor.de>
2018-09-13 17:01:20 +0200

    code cleanup

---
+++ Tests/Unit/ViewHelpers/SimplelinkViewHelperTest.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: frank
+ * Date: 13.09.18
+ * Time: 16:45
+ */
+
+namespace SUDHAUS7\Sudhaus7Formframework\Tests\Unit\ViewHelpers;
+
+use Nimut\TestingFramework\TestCase\UnitTestCase;
+
+
+class SimplelinkViewHelperTest
+{
+    
+    protected $mockedContentObjectRenderer;
+    protected $mockedViewHelper;
+    
+    /**
+     * Set up
+     */
+    public function setUp()
+    {
+    
+    }
+}
